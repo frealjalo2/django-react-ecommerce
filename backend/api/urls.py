46fb144a32bebb62e1573088e72b8d3247b32216
frealjalo2 <frealjaloude@gmail.com>
2025-03-05 21:11:35 +0100

    first commit

---
+++ backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from userauths import views as userauth_views
+from store import views as store_views
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('user/token/', userauth_views.MyTokenObtainPairView.as_view()),
+    path('user/token/refresh/', TokenRefreshView.as_view()),
+    path('user/register/', userauth_views.RegisterView.as_view()),
+    
+]
+
